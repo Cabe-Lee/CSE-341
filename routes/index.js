29d5e472f1043ab94fb3c390186e8719d3183a60
Cabe-Lee <cabetlee@gmail.com>
2026-01-31 19:29:26 -0800

    W04 Render test

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const routes = require('express').Router();
 // export from lesson1 controller
 const lesson1controller = require('../controllers/lesson1Con');
+
 // url routes
 routes.get('/kj', lesson1controller.nameRoute);
 routes.get('/stan', lesson1controller.stanRoute);
 
 routes.use('/contacts', require('./contacts'));
+
+routes.use('/', require('./swagger'));
+routes.use('/temples', require('./temple'));
+routes.use(
+    '/',
+    (docData = (req, res) => {
+        let docData = ({
+            documentationURL: 'localhost:8080',
+        });
+        res.json(docData);
+    })
+);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 // express web server & Mongo DB connection setup
 const express = require('express');
-const bodyParser = require('body-parser');
-const professionalRoutes = require('./routes/professional');
-const MongoClient = require('mongodb').MongoClient;
-const mongodb = require('./db/connect');
-
-const port = process.env.PORT || 8080;
-
 const app = express();
+const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+
+const professionalRoutes = require('./routes/professional');
+
+
+
+const swaggerAutogen = require('swagger-autogen');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .use(require('cors'))
+    .use(express.urlencoded({ extended: true }))
     .use(bodyParser.json())
     .use(express.static('frontend'))
     .use('/', require('./routes'))
@@ -21,14 +28,18 @@ app
     .use((req, res, next) => {
         res.status(404).send('Route not found. Please check the URL.');
     });
-
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(port);
-        console.log(`✅ Web Server is listening at port ${port}\n...`);
-    }
-});
+    
+const db = require('./models');
+db.mongoose
+  .connect(db.url)
+  .then(() => {
+    console.log('Connected to the database!');
+    app.listen(port);
+    console.log(`✅ Web Server is listening at port ${port}\n...`);
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the database!', err);
+    process.exit();
+  });
 
 console.log();
